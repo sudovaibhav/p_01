3329fc6c83a331d0dd5dee787262af83e456d986
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2026-01-16 22:25:56 +0530

    modif

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import sys
 if len(sys.argv)==2:
     script_name = sys.argv[0]
-    celsius = sys.argv[1]
+    celsius = float(sys.argv[1])
 else:
     script_name = sys.argv[0]
     celsius = 30
 print("Invalid inpur using default values.")
-f = (float(celsius) * 1.8) + 32
+f = (celsius * 1.8) + 32
 
-print("Celsius : ", celsius)
-print("Fahrenheit : ", f)
+print(f"Celsius : {celsius}")
+print(f"Fahrenheit : {f}")
