1cf1437c0d3c75666567a1a09c8a9054d42305e4
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2026-01-16 21:51:49 +0530

    file

---
+++ index.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv)==2:
+    script_name = sys.argv[0]
+    celsius = sys.argv[1]
+else:
+    script_name = sys.argv[0]
+    celsius = 30
+print("Invalid inpur using default values.")
+f = (celsius * 1.8) + 32
+
+print("Celsius : ", celsius)
+print("Fahrenheit : ", f)
